b0027bbaafa0d734d16f2f518fcc6dcf7995eae6
jaeil1004 <jaeil1004@gmail.com>
2021-09-04 16:38:23 +0900

    server 환경설정 및 eslint

---
+++ server/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'node': true,
+  },
+  'extends': [
+    'plugin:vue/essential',
+    'google',
+  ],
+  'parserOptions': {
+    'ecmaVersion': 12,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'vue',
+  ],
+  'rules': {
+    'linebreak-style': 0,
+    'eol-last': 0,
+  },
+};
