afad0520f7eddff7365f0ef39ef3eb9a885c1c37
jaeil1004 <jaeil1004@gmail.com>
2021-09-04 17:31:44 +0900

    setup of the express js server

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -17,5 +17,7 @@ module.exports = {
   'rules': {
     'linebreak-style': 0,
     'eol-last': 0,
+    'new-cap': 0,
+    'comma-dangle': 0,
   },
 };
--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,24 @@
-console.log('Hello World');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+const router = express.Router();
+app.set('port', process.env.PORT || 8080);
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('combined'));
+
+
+app.route('/status').get(function(req, res) {
+  res.send({
+    message: 'Hello world',
+  });
+});
+
+app.use('/', router);
+
+app.listen(app.get('port'), function() {
+  console.log('Express Server Started !!!');
+});
